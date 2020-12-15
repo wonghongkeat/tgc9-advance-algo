31f95dc2286a277775e42de4afc2234c0c0530de
wonghongkeat <hongkeat1991@gmail.com>
2020-12-15 04:04:56 +0000

    edited

---
+++ queue-example/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <QueueExample/>
+  </div>
+</template>
+
+<script>
+import QueueExample from './components/QueueExample'
+
+export default {
+  name: 'App',
+  components: {
+      QueueExample
+  }
+}
+</script>
+
+<style>
+
+</style>
