91617505e96ca5b8c87a0e324fa1b32786962d82
wonghongkeat <hongkeat1991@gmail.com>
2020-12-15 06:26:23 +0000

    edited

--- queue-example/src/App.vue
+++ queue-example/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
       <QueueExample/>
+      <Animation/>
   </div>
 </template>
 
 <script>
-import QueueExample from './components/QueueExample'
+import QueueExample from './components/QueueExample';
+import Animation from './components/Animation';
 
 export default {
   name: 'App',
   components: {
-      QueueExample
+      QueueExample,
+      Animation
   }
 }
 </script>
---
+++ stack-example/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+   <StackExample/>
+  </div>
+</template>
+
+<script>
+
+import StackExample from './components/StackExample'
+export default {
+  name: 'App',
+  components: {
+      StackExample
+  }
+}
+</script>
+
+<style>
+
+</style>
